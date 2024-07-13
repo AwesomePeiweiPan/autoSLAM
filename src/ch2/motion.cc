1e807fa34c680858d4baf1385cc1c71e056f1965
AwesomePeiweiPan <913746703ppw@gmail.com>
2024-07-13 09:55:22 +0200

    CH2 comments

--- src/ch2/motion.cc
+++ src/ch2/motion.cc
@@ -2,24 +2,30 @@
 // Created by xiang on 22-12-29.
 //
 
-#include <gflags/gflags.h>
-#include <glog/logging.h>
+#include <gflags/gflags.h>   //允许程序定义命令行标志并且解析他们 对应下面的ParseCommandLineFlags(&argc,&argb,true);
+#include <glog/logging.h>    //提供日志级别，并且可以将日志信息输出到控制台或文件:LOG(INFO);LOG(WARNING);LOG(ERROR);LOG(FATAL)
+                             //初始化：InitGoogleLogging,FLAGS_stderrthreshold;FLAGS_colorlogtostderr
+                             //记录日志：LOG(INFO)
 
-#include "common/eigen_types.h"
-#include "common/math_utils.h"
-#include "tools/ui/pangolin_window.h"
+#include "common/eigen_types.h"           //自己定义的头文件，里面包含了Eigen库的头文件
+#include "common/math_utils.h"            //自己定义的头文件，包含库的头文件
+#include "tools/ui/pangolin_window.h"     //自己定义的头文件，包含库的头文件
 
 /// 本节程序演示一个正在作圆周运动的车辆
 /// 车辆的角速度与线速度可以在flags中设置
 
-DEFINE_double(angular_velocity, 10.0, "角速度（角度）制");
+//gflags库的自定义参数
+DEFINE_double(angular_velocity, 10.0, "角速度（角度）制");    
 DEFINE_double(linear_velocity, 5.0, "车辆前进线速度 m/s");
 DEFINE_bool(use_quaternion, false, "是否使用四元数计算");
 
 int main(int argc, char** argv) {
-    google::InitGoogleLogging(argv[0]);
-    FLAGS_stderrthreshold = google::INFO;
-    FLAGS_colorlogtostderr = true;
+    google::InitGoogleLogging(argv[0]);     //程序的名称或者路径，方便初始化日志的时候作为日志的标识符
+    FLAGS_stderrthreshold = google::INFO;   //日志的级别，INFO，WARNING等
+    FLAGS_colorlogtostderr = true;          //彩色日志输出
+    //argc argument count,命令行的参数数量;./program -a -b value, argc=4;
+    //argv argument vector,命令行的参数;argv[0]:程序名称或者路径; argv[1]到argv[argc-1]:传递给程序的其他命令行参数
+    //true:argv移除已处理的标志
     google::ParseCommandLineFlags(&argc, &argv, true);
 
     /// 可视化
@@ -29,9 +35,9 @@ int main(int argc, char** argv) {
     }
 
     double angular_velocity_rad = FLAGS_angular_velocity * sad::math::kDEG2RAD;  // 弧度制角速度
-    SE3 pose;                                                                    // TWB表示的位姿
-    Vec3d omega(0, 0, angular_velocity_rad);                                     // 角速度矢量
-    Vec3d v_body(FLAGS_linear_velocity, 0, 0);                                   // 本体系速度
+    SE3 pose;                                                                    // TWB表示的位姿  //Sophus库，四元数表示旋转
+    Vec3d omega(0, 0, angular_velocity_rad);                                     // 角速度矢量     //Eigen库
+    Vec3d v_body(FLAGS_linear_velocity, 0, 0);                                   // 本体系速度     //Eigen库
     const double dt = 0.05;                                                      // 每次更新的时间
 
     while (ui.ShouldQuit() == false) {
@@ -41,11 +47,13 @@ int main(int argc, char** argv) {
 
         // 更新自身旋转
         if (FLAGS_use_quaternion) {
+            // unit_quaternion()转化为单位四元数
             Quatd q = pose.unit_quaternion() * Quatd(1, 0.5 * omega[0] * dt, 0.5 * omega[1] * dt, 0.5 * omega[2] * dt);
+            // 归一化为单位四元数
             q.normalize();
-            pose.so3() = SO3(q);
+            pose.so3() = SO3(q); //一个是四元数q，一个是SO3，还是有区别的
         } else {
-            pose.so3() = pose.so3() * SO3::exp(omega * dt);
+            pose.so3() = pose.so3() * SO3::exp(omega * dt);  //SO3::exp(omega * dt)直接转换成四元数
         }
 
         LOG(INFO) << "pose: " << pose.translation().transpose();
--- src/common/nav_state.h
+++ src/common/nav_state.h
@@ -5,8 +5,8 @@
 #ifndef SAD_NAV_STATE_H
 #define SAD_NAV_STATE_H
 
-#include <sophus/so3.hpp>
-#include "common/eigen_types.h"
+#include <sophus/so3.hpp>             //Sophus库中的SO3
+#include "common/eigen_types.h"       //Eigen库中的矩阵类型
 
 namespace sad {
 
@@ -16,25 +16,42 @@ namespace sad {
  *
  * 这是个封装好的类，部分程序使用本结构体，也有一部分程序使用散装的pvq，都是可以的
  */
+
+//模版类结构体
 template <typename T>
 struct NavState {
+    //定义三维变量 类型 的别名
     using Vec3 = Eigen::Matrix<T, 3, 1>;
     using SO3 = Sophus::SO3<T>;
 
+    //默认构造函数，不会初始化类的成员变量
     NavState() = default;
 
     // from time, R, p, v, bg, ba
+    //显式调用，调用的时候必须指明参数: sad::NavState<double> state1(123.456, R, t, v, bg, ba); 
     explicit NavState(double time, const SO3& R = SO3(), const Vec3& t = Vec3::Zero(), const Vec3& v = Vec3::Zero(),
                       const Vec3& bg = Vec3::Zero(), const Vec3& ba = Vec3::Zero())
         : timestamp_(time), R_(R), p_(t), v_(v), bg_(bg), ba_(ba) {}
 
     // from pose and vel
+    //隐式调用，调用的时候不一定需要指明参数，但是会有风险: sad::NavStated state2 = {123.456, pose};
     NavState(double time, const SE3& pose, const Vec3& vel = Vec3::Zero())
         : timestamp_(time), R_(pose.so3()), p_(pose.translation()), v_(vel) {}
 
     /// 转换到Sophus
     Sophus::SE3<T> GetSE3() const { return SE3(R_, p_); }
 
+    //友元函数，保证可以访问私有的变量，即使这里都是公有的，方便以后做扩展
+    //返回值和函数名
+        //std::ostream&     返回值为一个输出流类型的引用    
+        //operator<<        重载了<<操作符
+    //函数参数
+        //std:ostream& os   输出流对象，例如 std::cout
+        //const NavState    常量引用，要输出的NavState对象
+    //返回值
+        //返回输出流os，以便进行链式操作
+    //使用
+        //std::cout << state << std::endl;
     friend std::ostream& operator<<(std::ostream& os, const NavState<T>& s) {
         os << "p: " << s.p_.transpose() << ", v: " << s.v_.transpose()
            << ", q: " << s.R_.unit_quaternion().coeffs().transpose() << ", bg: " << s.bg_.transpose()
@@ -42,6 +59,7 @@ struct NavState {
         return os;
     }
 
+    //公有变量
     double timestamp_ = 0;    // 时间
     SO3 R_;                   // 旋转
     Vec3 p_ = Vec3::Zero();   // 平移
@@ -50,6 +68,8 @@ struct NavState {
     Vec3 ba_ = Vec3::Zero();  // acce 零偏
 };
 
+//定义在struct之外，在namespace之内，方便使用
+//例如: ui.UpdateNavState(sad::NavStated(0, pose, v_world));
 using NavStated = NavState<double>;
 using NavStatef = NavState<float>;
 
